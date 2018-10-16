a671c3e44d0670dd5bf6df83727fd3494c526d7f
Magnus Wikhög <mawi1402@student.miun.se>
2018-10-16 10:22:39 +0200

    Added sample code in MainActivity

--- app/src/main/java/com/magnuswikhog/devlogproject/MainActivity.java
+++ app/src/main/java/com/magnuswikhog/devlogproject/MainActivity.java
@@ -3,11 +3,19 @@ package com.magnuswikhog.devlogproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.magnuswikhog.devlog.DevLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DevLog.remoteLoggingEnabled = false;
+        DevLog.loggingEnabled = true;
+        DevLog.tagPrefix = "DevLogProject ";
+        DevLog.i("MainActivity", "Hello World!");
+        // DevLog.storeRemoteLog(this); Use every now and then if remote logging is enabled, to send logs to server
     }
 }
